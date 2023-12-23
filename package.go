e729b01b11e3464a5ceb1bd518b9a5b06d23f2eb
Lokesh Naik <54623523+Lokesh-naik01@users.noreply.github.com>
2023-12-23 18:43:13 +0530

    feat(doc): add notes about constants and input from console
    feat(doc): add notes about constants and input from console
    #2

--- package.go
+++ package.go
@@ -1,7 +1,26 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello World");
 	fmt.Println(`back ticks can be used to print`);
+
+	const investmentAmount = 1000
+	// constant can't be changed
+	var expectedReturnRate float64= 5.5
+	var years float64= 10
+
+	fmt.Print("Input the number of years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Println("Future value :",  futureValue, " years ", years)
+	fmt.Printf("futureValue: %.2f number of years are %v \n", futureValue, years)
+
+	formattedFV := fmt.Sprintf("Future value: %.2f", futureValue)
+	fmt.Print(formattedFV, "\nNumber of years ", years)
+
 }
